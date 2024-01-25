1c84aa5a13868f28d4a8bd75a50e11d5825e3224
Thomas NACHAR <thomas.nachar18@gmail.com>
2024-01-25 11:40:10 +0100

    build6

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    outDir: 'dist',
-  },
-  base: '/bvg',
   plugins: [
     vue(),
   ],
